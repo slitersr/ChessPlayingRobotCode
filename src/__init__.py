3520a67486e50c6bb972ecbeee62a493cf540a2c
slitersr <slitersr@mail.uc.edu>
2022-01-26 20:06:21 -0500

    game functions via terminal, need error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,14 @@
 import chess
 import chess.engine
 import waitForInput
+import string
+
 
 
 def main():
 
     #SimpleEngine spawns event loop and creates engine instance using stockfish.exe
-    engine = chess.engine.SimpleEngine.popen_uci(r"C:/Users/seans/Desktop/SeniorDesignI/ProjectCode/src/stockfish_14_win_x64_avx2/stockfish_14_win_x64_avx2.exe")
+    engine = chess.engine.SimpleEngine.popen_uci(r"C:/Users/seans/Desktop/SeniorDesignI/ProjectCode/src/stockfish_14_win_x64_avx2/stockfish_14_x64_avx2.exe")
 
     #create board
     board = chess.Board()
@@ -14,33 +16,60 @@ def main():
     #player will always play as white for now, will start with first move
     playerTurn = True
 
+    # robot.reset()
+
     #while game is not over, keep looping
     while (not board.is_game_over()) and (not board.is_checkmate()) and (not board.is_stalemate()):
 
         #if it is players turn have player enter move/ move will be played by arm
         if(playerTurn):
+            inputVar = input("Press enter to start recording...")
+
             #read input from user
             playerMoveText = waitForInput.microphoneReady()
 
+            #make lowercase text
+            playerMoveText = playerMoveText.lower()
+
             #get rid of any whitespace in voice inputted string
-            playerMoveText.replace(" ", "")
+            playerMoveText = playerMoveText.replace(' ', '')
 
             #if player inputted move is not legal then loop back and ask for move again
-            while(not chess.Move.from_uci(player_move) in board.legal_moves):
-                player_move = input("Enter move")
+            while(chess.Move.from_uci(playerMoveText) not in board.legal_moves):
+                inputVar = input("Press enter to start recording...")
+                playerMoveText = waitForInput.microphoneReady()
 
+            
             # make sure move satisfies mate condition. If not pick new move
-            board.push(player_move)
+            board.push_san(playerMoveText)
+
+            
+            # here is where we would move the physical board peice
+            # firstHalfplayerMoveText = playerMoveText.split(' ')[0]
+            # secondHalfplayerMoveText = playerMoveText.split(' ')[1]
+            #robot.move(firstHalfplayerMoveText, secondHalfplayerMoveText)
+
+            print("\nPLAYER MOVE\n")
+
+
+            print(board)
 
             #make it the engine's turn after the player has gone
             playerTurn = False
 
         #if it's the robots turn, calculate engineResult.move and perform move with robot
         else:   
+
+            # see if engine is in check  here.
+
+
             # evaluate best move
             engineResult = engine.play(board, chess.engine.Limit(time=0.1))
             # perform move
             board.push(engineResult.move)
+
+            print("\nCOMPUTER MOVE\n")
+            print(board)
 
             #make it the players move again
             playerTurn = True
@@ -58,3 +87,10 @@ def main():
     #end of the game
     engine.quit()
 
+
+
+
+
+
+
+main()
--- src/waitForInput.py
+++ src/waitForInput.py
@@ -8,31 +8,25 @@ def microphoneReady():
     #initializes instance of library used to read in audio from mic
     r = sr.Recognizer()
     
+    validInput = False
+
     #for as long as the user is speaking/the microphone volume is picked up beyond threshold, audio is turned into text with r.listen
     with sr.Microphone() as source:
-        # read the audio data from the default microphone
-        audio = r.listen(source)
-        print("Recognizing...")
+        while(not validInput):
+            # read the audio data from the default microphone
+            audio = r.listen(source)
+            print("Recognizing...")
+            # verify input is valid
+            try:
+                print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-        # verify input is valid
-        try:
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-        # if input is valid, turn audio into text variable
-        text = r.recognize_google(audio)
-        print(text)
-        
-        #write text to playerInput.txt
-        with open('C:/Users/seans/Desktop/SeniorDesignI/ProjectCode/' + 'playerInput.txt', 'w') as f:
-            f.write(text)
-            f.close
-
+            # if input is valid, turn audio into text variable
+            text = r.recognize_google(audio)
+            print(text)
+            #valid input was inputted
+            validInput = True
     return text
-
-
-
-microphoneReady()
